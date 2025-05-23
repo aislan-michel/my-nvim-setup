a292881b0b258a6ec1e4113aefbde4073ff449c4
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2025-05-23 18:58:47 -0300

    feat: add treesitter

--- init.lua
+++ init.lua
@@ -25,14 +25,24 @@ local plugins = {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  { "nvim-treesitter/nvim-treesitter", build=":TSUpdate" }
 }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config = require("nvim-treesitter.configs") 
+config.setup({
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "c_sharp" },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },  
+  })
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
