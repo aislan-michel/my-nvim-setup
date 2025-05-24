b47eca003ba07a704ec290f1610d30e14bb1bf50
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2025-05-23 21:09:26 -0300

    feat: modularizing

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -20,21 +14,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("vim-options")
 require("lazy").setup("plugins")
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
-vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal right<CR>", {});
 
-local config = require("nvim-treesitter.configs") 
-config.setup({
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "c_sharp" },
-    sync_install = false,
-    highlight = { enable = true },
-    indent = { enable = true },  
-  })
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
