038a8349e1322172a9dcad185a2b7bbf817122db
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2025-05-24 15:09:35 -0300

    feat: configure null-ls

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs") 
     config.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "c_sharp" },
+      auto_install = true,
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },  
